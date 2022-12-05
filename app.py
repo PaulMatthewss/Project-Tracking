f81a6a8fdd9e6a84729c3cd7604fdd3e7763b1fb
Paul Matthews <paulyshkolychistoe@gmail.com>
2022-12-05 13:53:43 +0300

    basic navbar and empty pages

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return "Hello, world!"
+    return render_template('index.html')
+
+@app.route('/workers')
+def workers():
+    return render_template('workers.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/newworker')
+def newworker():
+    return render_template('newworker.html')
+
+@app.route('/newproject')
+def newproject():
+    return render_template('newproject.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
